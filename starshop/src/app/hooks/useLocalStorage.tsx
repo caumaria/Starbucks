5b040e279d49b4da639580790392a8e69e4b1ee4
Cau <cau.ugolini@gmail.com>
2023-10-18 17:26:59 -0300

    fix<localStorage>: reference error

--- starshop/src/app/hooks/useLocalStorage.tsx
+++ starshop/src/app/hooks/useLocalStorage.tsx
@@ -2,19 +2,23 @@ import { useEffect, useState } from "react";
 
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
   const [value, setValue] = useState<T>(() => {
- 
-    const jsonValue = localStorage.getItem(key);
-    if (jsonValue != null) return JSON.parse(jsonValue); 
-    
-    if (typeof initialValue === "function") {
-      return (initialValue as () => T)();
-    } else {
-      return initialValue;
+
+    if (typeof window !== "undefined") {
+      const jsonValue = localStorage.getItem(key);
+      if (jsonValue != null) return JSON.parse(jsonValue);
+
+      if (typeof initialValue === "function") {
+        return (initialValue as () => T)();
+      } else {
+        return initialValue;
+      }
     }
   });
 
-  useEffect(() => {    
-      localStorage.setItem(key, JSON.stringify(value));    
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
   }, [key, value]);
 
   return [value, setValue] as [typeof value, typeof setValue];
